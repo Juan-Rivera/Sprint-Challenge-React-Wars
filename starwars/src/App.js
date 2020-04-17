d6210d18d68bf3155aebb1bd35b298e861eec1aa
Juan (programming) <programjuan@Juans-MacBook-Pro.local>
2020-04-17 12:49:07 -0400

    commit test

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character' //imports the Character.js file
+import { getCharacter } from 'rickmortyapi' //getCharacter() which gets all characters as a promise (axios)
+
+
+const url = 'https://rickandmortyapi.com/api/character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +15,20 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState(null);
+
+  const characterData = useEffect(() => {
+    getCharacter()
+    .then((res) =>
+    //console.log(res.results)
+    setCharacters(res.results)
+    )
+  }, []) 
+//commit
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character />
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,19 @@
 // Write your Character component here
+import React from 'react'
+
+
+export default function Character({Character}) {
+return(
+    <div className='characters-container'>
+        <div className='img-container'>
+            
+            <p>Hello World</p>
+        </div>
+
+        <div className='character-info'>
+          
+        </div>
+
+    </div>
+)
+}
